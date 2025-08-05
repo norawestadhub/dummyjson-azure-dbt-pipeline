0d4ac2d934629063f7adacfc674f9ed95c03afb1
norawestadhub <noratw@hotmail.com>
2025-08-05 15:43:57 +0200

    Add Azure AD Storage Integration and incremental load to Snowflake pipeline

--- sql/tasks/01_create_task.sql
+++ sql/tasks/01_create_task.sql
@@ -1,76 +1,97 @@
--- 1) Velg database og schema
+-- 0) Velg database og schema
 USE DATABASE DUMMYJSON_PIPELINE;
 USE SCHEMA RAW_STAGE;
 
--- 2) (Re-)opprett JSON-filformat
+-- 1) (Re-)opprett JSON-filformat
 CREATE OR REPLACE FILE FORMAT RAW_JSON_FORMAT
-  TYPE       = 'JSON'
+  TYPE        = 'JSON'
   COMPRESSION = 'AUTO'
 ;
 
--- 3) (Re-)opprett stage mot Azure Blob (ideelt burde man bruke storage integration med Azure AD, i stedet for å bruke azure sas token direkte slik jeg har gjort for å spare tid)
-CREATE OR REPLACE STAGE RAW_JSON_STAGE
-  URL = 'azure://dummyjsonstorage01.blob.core.windows.net/raw'
-  CREDENTIALS = (
-    AZURE_SAS_TOKEN = 'sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2025-08-04T20:07:13Z&st=2025-08-04T11:52:13Z&spr=https&sig=8MTfueW7U%2BVVE7BPN35%2BwTqTg6KhlLVeCPyVwWIJJoI%3D'
+-- 2) (Re-)opprett Storage Integration mot Azure Blob
+CREATE OR REPLACE STORAGE INTEGRATION MY_AZURE_INT
+  TYPE                      = EXTERNAL_STAGE
+  STORAGE_PROVIDER          = 'AZURE'
+  ENABLED                   = TRUE
+  AZURE_TENANT_ID           = '53b76c82-3f7e-4d92-9e49-7e1da18b8696'
+  STORAGE_ALLOWED_LOCATIONS = (
+    'azure://dummyjsonstorage01.blob.core.windows.net/raw'
   )
-  FILE_FORMAT = RAW_JSON_FORMAT
 ;
 
--- 4) (Re-)opprett råtabeller
-CREATE OR REPLACE TABLE PRODUCTS_RAW (json_data VARIANT);
-CREATE OR REPLACE TABLE CARTS_RAW    (json_data VARIANT);
-CREATE OR REPLACE TABLE USERS_RAW    (json_data VARIANT);
+-- 3) Inspeksjon av integrasjonen
+DESCRIBE STORAGE INTEGRATION MY_AZURE_INT;
 
--- 5) Test at manuelle COPY fungerer
+-- 4) (Re-)opprett stage mot Azure Blob ved å bruke integrasjonen
+CREATE OR REPLACE STAGE RAW_JSON_STAGE
+  URL                 = 'azure://dummyjsonstorage01.blob.core.windows.net/raw'
+  STORAGE_INTEGRATION = MY_AZURE_INT
+  FILE_FORMAT         = RAW_JSON_FORMAT
+;
+
+-- 5) (Re-)opprett råtabeller
+-- CREATE OR REPLACE TABLE PRODUCTS_RAW (json_data VARIANT);
+-- CREATE OR REPLACE TABLE CARTS_RAW    (json_data VARIANT);
+-- CREATE OR REPLACE TABLE USERS_RAW    (json_data VARIANT);
+
+-- 6) Test og hent filnavn
+LIST @RAW_JSON_STAGE;
+LIST @RAW_JSON_STAGE PATTERN = '.*products_.*[.]json$';
+LIST @RAW_JSON_STAGE PATTERN = '.*carts_.*[.]json$';
+LIST @RAW_JSON_STAGE PATTERN = '.*users_.*[.]json$';
+
+-- 7) COPY til råtabeller med FORCE=FALSE for kun nye filer
 COPY INTO PRODUCTS_RAW (json_data)
   FROM @RAW_JSON_STAGE
   FILE_FORMAT = RAW_JSON_FORMAT
-  PATTERN     = '.*raw/products_.*\.json$'
-  ON_ERROR    = 'CONTINUE'
-;
-COPY INTO CARTS_RAW (json_data)
-  FROM @RAW_JSON_STAGE
-  FILE_FORMAT = RAW_JSON_FORMAT
-  PATTERN     = '.*raw/carts_.*\.json$'
-  ON_ERROR    = 'CONTINUE'
-;
-COPY INTO USERS_RAW (json_data)
-  FROM @RAW_JSON_STAGE
-  FILE_FORMAT = RAW_JSON_FORMAT
-  PATTERN     = '.*raw/users_.*\.json$'
+  PATTERN     = '.*products_.*[.]json$'
+  FORCE       = FALSE
   ON_ERROR    = 'CONTINUE'
 ;
 
--- 6) Opprett eller oppdater en schedule-task som kjører hver mandag kl 09:00 UTC
+COPY INTO CARTS_RAW (json_data)
+  FROM @RAW_JSON_STAGE
+  FILE_FORMAT = RAW_JSON_FORMAT
+  PATTERN     = '.*carts_.*[.]json$'
+  FORCE       = FALSE
+  ON_ERROR    = 'CONTINUE'
+;
+
+COPY INTO USERS_RAW (json_data)
+  FROM @RAW_JSON_STAGE
+  FILE_FORMAT = RAW_JSON_FORMAT
+  PATTERN     = '.*users_.*[.]json$'
+  FORCE       = FALSE
+  ON_ERROR    = 'CONTINUE'
+;
+
+-- 8) Opprett eller oppdater task som kjører hver mandag kl 11:00 UTC
 CREATE OR REPLACE TASK RAW_STAGE_LOAD_TASK
   WAREHOUSE = RAW_LOAD_WH
-  SCHEDULE  = 'USING CRON 0 11 * * 1 UTC'  -- mandag kl 11:00 UTC
+  SCHEDULE  = 'USING CRON 0 11 * * 1 UTC'
 AS
   COPY INTO PRODUCTS_RAW (json_data)
     FROM @RAW_JSON_STAGE
     FILE_FORMAT = RAW_JSON_FORMAT
-    PATTERN     = '.*raw/products_.*\.json$'
+    PATTERN     = '.*products_.*[.]json$'
+    FORCE       = FALSE
     ON_ERROR    = 'CONTINUE'
   ;
   COPY INTO CARTS_RAW (json_data)
     FROM @RAW_JSON_STAGE
     FILE_FORMAT = RAW_JSON_FORMAT
-    PATTERN     = '.*raw/carts_.*\.json$'
+    PATTERN     = '.*carts_.*[.]json$'
+    FORCE       = FALSE
     ON_ERROR    = 'CONTINUE'
   ;
   COPY INTO USERS_RAW (json_data)
     FROM @RAW_JSON_STAGE
     FILE_FORMAT = RAW_JSON_FORMAT
-    PATTERN     = '.*raw/users_.*\.json$'
+    PATTERN     = '.*users_.*[.]json$'
+    FORCE       = FALSE
     ON_ERROR    = 'CONTINUE'
   ;
 
--- 7) Aktiver tasken
+-- 9) Aktiver tasken og verifiser
 ALTER TASK RAW_STAGE_LOAD_TASK RESUME;
-
--- 8) Sjekk at tasken er aktiv
 SHOW TASKS LIKE 'RAW_STAGE_LOAD_TASK';
-
-ALTER TASK RAW_STAGE_LOAD_TASK
-  SUSPEND;
